66ad591ca798b81ba5c73403c621394a3e2b6a26
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2021-10-03 10:44:39 -0500

    Complete Chapt 3

--- Chapter 3/sizer.cpp
+++ Chapter 3/sizer.cpp
@@ -12,8 +12,19 @@ int main()
     std::cout << sizeof(long) << " bytes\n";
 
     std::cout << "The size of a character:\t";
-    std::cout << sizeof(char) << " bytes\n"
+    std::cout << sizeof(char) << " bytes\n";
 
     std::cout << "The size of a boolean:\t";
     std::cout << sizeof(bool) << " bytes\n";
+
+    std::cout << "The size of a float:\t";
+    std::cout << sizeof(float) << " bytes\n";
+
+    std::cout << "The size of a double float:\t";
+    std::cout << sizeof(double) << " bytes\n";
+
+    std::cout << "The size of a long long int:\t";
+    std::cout << sizeof(long long int) << " bytes\n";
+
+    return 0;
 }
