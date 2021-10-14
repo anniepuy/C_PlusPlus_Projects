33a1a6a98498f92fbabfbcd2f4f053507ba7898d
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2021-10-13 23:10:54 -0500

    Using heap.cpp to test assigning variables with values to the heap memory via pointers

---
+++ Chapter10/heap.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int localVariable = 5;
+    int *pLocal = &localVariable;
+    int *pHeap = new int;
+    if (pHeap == nullptr)
+    {
+        cout << "Error! No memory for the pHeap!";
+        return 1;
+    }
+    *pHeap = 7;
+    cout << "localVariable: " << localVariable << "\n";
+    cout << "*pLocal: " << *pLocal << "\n";
+    cout << "*pHeap: " << *pHeap << "\n";
+    delete pHeap;
+    pHeap = new int;
+    if (pHeap == nullptr)
+    {
+        cout << "Error! No memory for the pHeap!";
+        return 1;
+    }
+    *pHeap = 9;
+    cout << "*pHeap: " << *pHeap << "\n";
+    delete pHeap;
+    return 0;
+
+}
