88c2dd047ae0bdf365bb63ce9358aeb3a1d5beb1
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2021-10-18 18:19:59 -0500

    Assigning a reference to an integer in reference.cpp

---
+++ Chapter12/reference.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+//adds a reference to an int initialized
+int main()
+{
+    int intOne;
+    int &rSomeRef = intOne;
+
+    intOne = 5;
+    cout << "intOne: " << intOne << endl;
+    cout << "rSomeRef: " << rSomeRef << endl;
+
+    rSomeRef = 7;
+    cout << "intOne: " << intOne << endl;
+    cout << "rSomeRef: " << rSomeRef << endl;
+
+    return 0;
+}
